93d03343408da9c7c7f61832d26d97a9bfe4d35f
fj-fj-fj <x1x1xx0@protonmail.com>
2021-12-11 01:12:48 +0300

    add menu structure

---
+++ app/menu.py
@@ -0,0 +1,43 @@
+menu = [
+    {
+        'name': 'Rotem information',
+        'url': 'about',
+    },
+    {
+        'name': 'Rotem test details',
+        'url': 'tests',
+    },
+    {
+        'name': 'Historical data (ТЭГ/Rotem)',
+        'url': 'historical_data',
+    },
+    {
+        'name': 'Rotem parameters',
+        'url': 'parameters',
+    },
+    {
+        'name': 'video instructions',
+        'select': [
+            {
+                'name': 'Work',
+                'url': 'video_instructions_work',
+            },
+            {
+                'name': 'smth(?)',
+                'url': 'video_instructions_smth',
+            },
+        ],
+    },
+    {
+        'name': 'Interpretation of results',
+        'url': 'results_interpretation',
+    },
+    {
+        'name': 'Help service',
+        'url': 'help',
+    },
+    {
+        'name': 'Useful links',
+        'url': 'useful_links',
+    },
+]
