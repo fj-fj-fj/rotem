b87c99317d3cd980062eed6981f549d4285c7e81
fj-fj-fj <x1x1xx0@protonmail.com>
2021-12-19 20:17:23 +0300

    new: create scroll to top button

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -7,3 +7,12 @@ $('.menu__btn').on('click', function(e) {
     $('.menu__btn').toggleClass('menu__btn__active');
     $('.menu__btn').toggleClass('change');
 });
+
+const scrollUp = document.querySelector('.top');
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 100) {
+        scrollUp.classList.add('active');
+    } else {
+        scrollUp.classList.remove('active');
+    }
+});
