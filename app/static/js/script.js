0c0ff65c5f956472eb464fe9c14963d1036503c0
fj-fj-fj <x1x1xx0@protonmail.com>
2021-12-25 18:00:29 +0300

    by, jquery

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,18 +1,27 @@
 'use strict';
 
-$('.menu__btn').on('click', function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('menu__active');
-    $('.content').toggleClass('content__active');
-    $('.menu__btn').toggleClass('menu__btn__active');
-    $('.menu__btn').toggleClass('change');
-});
 
-const scrollUp = document.querySelector('.top');
-window.addEventListener('scroll', function() {
-    if (window.pageYOffset > 100) {
-        scrollUp.classList.add('active');
-    } else {
-        scrollUp.classList.remove('active');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+
+    const toggleCSSClasses = (el, ...cls) => cls.map(cl => el.classList.toggle(cl)),
+        container = document.querySelector('.container'),
+        menu = container.querySelector('.menu'),
+        content = container.querySelector('.content'),
+        menuButton = menu.querySelector('.menu__btn'),
+        scrollUp = container.querySelector('.top');
+
+    menuButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        menu.classList.toggle('menu__active');
+        content.classList.toggle('content__active');
+        toggleCSSClasses(menuButton, 'menu__btn__active', 'change');
+    });
+
+    document.addEventListener('scroll', () => {
+        if (window.pageYOffset > 100) {
+            scrollUp.classList.add('active');
+        } else {
+            scrollUp.classList.remove('active');
+        }
+    });
 });
